697b5830f1bdca3645611c305c65fca90c520d80
Ivanor1505 <ivanor1505@gmail.com>
2023-08-23 19:01:49 +0300

    step 4

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import { StatisticsBox } from "./Statistics. styled";
+import { Notification } from 'components/Notification/Notification';
 
 export class Statistics extends Component{
     render() {
         const { good, neutral, bad, total, positivePercentage } = this.props;
 
-        return (
+        return (total?
             <StatisticsBox>
             <p>Good: {good}</p>
             <p>Neutral: {neutral}</p>
             <p>Bad: {bad}</p>
             <p>Total: {total}</p>
             <p>Positive feedback: {positivePercentage}%</p>
-            </StatisticsBox> 
+            </StatisticsBox> :
+            <Notification message="There is no feedback" />
                 );
             }
 }
